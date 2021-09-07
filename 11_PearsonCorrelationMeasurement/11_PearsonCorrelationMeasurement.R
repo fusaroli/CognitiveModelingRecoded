ab5f118e46f0db4f38f6311f5a0ca13cf354039e
fusaroli <fusaroli@gmail.com>
2021-09-07 12:28:42 +0200

    add 11_PearsonCorrelationMeasurement

---
+++ 11_PearsonCorrelationMeasurement/11_PearsonCorrelationMeasurement.R
@@ -0,0 +1,124 @@
+pacman::p_load(
+  tidyverse,
+  here,
+  cmdstanr,
+  posterior,
+  brms,
+  bayesplot
+)
+
+x <- matrix(c( .8, 102, 
+               1.0,  98, 
+               .5, 100,
+               .9, 105, 
+               .7, 103, 
+               .4, 110,
+               1.2,  99, 
+               1.4,  87,
+               .6, 113,
+               1.1,  89,
+               1.3,  93), nrow=11, ncol=2, byrow=T) 
+
+n <- nrow(x) # number of people/units measured
+
+# precision of measurement:
+sigmaerror = c(.03, 1)
+# sigmaerror = c(.03, 10)
+
+data <- list(x=x, n=n, sigmaerror=sigmaerror) # to be passed on to Stan
+
+df <- tibble(x1=x[,1], x2 = x[,2], se1=sigmaerror[1], se2=sigmaerror[2])
+
+## Specify where the model is
+file <- file.path(here("11_PearsonCorrelationMeasurement", "11_PearsonCorrelationMeasurement.stan"))
+mod <- cmdstan_model(file, cpp_options = list(stan_threads = TRUE))
+
+samples <- mod$sample(
+  data = data,
+  seed = 123,
+  chains = 2,
+  parallel_chains = 2,
+  threads_per_chain = 2,
+  iter_warmup = 2000,
+  iter_sampling = 2000,
+  refresh = 500,
+  max_treedepth = 20,
+  adapt_delta = 0.99,
+)
+
+samples$summary()  # summary, same as print(samples)
+
+# Extract posterior samples 
+draws_df <- as_draws_df(samples$draws()) 
+
+# Now let's plot a histogram for mu and sigma. 
+ggplot(draws_df) +
+  geom_density(aes(rprior), fill="red", alpha=0.3) +
+  geom_density(aes(`r`), fill="blue", alpha=0.3) +
+  xlab("Pearson coefficient") +
+  ylab("Posterior Density") +
+  theme_classic()
+
+ggplot(draws_df) +
+  geom_density(aes(`muprior`), fill="red", alpha=0.3) +
+  geom_density(aes(`mu[1]`), fill="blue", alpha=0.3) +
+  geom_density(aes(`mu[2]`), fill="green", alpha=0.3) +
+  xlab("Mean") +
+  ylab("Posterior Density") +
+  theme_classic()
+
+ggplot(draws_df) +
+  geom_density(aes(`sigmaprior`), fill="red", alpha=0.3) +
+  geom_density(aes(`sigma[1]`), fill="blue", alpha=0.3) +
+  geom_density(aes(`sigma[2]`), fill="green", alpha=0.3) +
+  xlab("Sigma") +
+  ylab("Posterior Density") +
+  theme_classic()
+
+ggplot(draws_df, aes(`mu[1]`, `sigma[1]`)) +
+  geom_point(alpha=0.1) +
+  xlab("Mean") +
+  ylab("Standard Deviation") +
+  geom_rug(size = 0.1) +
+  theme_classic()
+
+ggplot(draws_df, aes(`mu[2]`, `sigma[2]`)) +
+  geom_point(alpha=0.1) +
+  xlab("Mean") +
+  ylab("Standard Deviation") +
+  geom_rug(size = 0.1) +
+  theme_classic()
+
+## Diagnostics
+samples$cmdstan_diagnose() 
+stanfit <- rstan::read_stan_csv(samples$output_files())
+posterior_cp <- as.array(stanfit)
+
+mcmc_trace(posterior_cp)
+mcmc_rank_overlay(posterior_cp)
+
+
+## Now in brms
+m <- brm(
+  bf(x1|se(se1, sigma = TRUE) ~ 1) + 
+    bf(x2|se(se2, sigma = TRUE) ~ 1) + 
+    set_rescor(rescor = TRUE),
+  df,
+  family = gaussian,
+  prior = c(
+    prior(normal(0, 10), class = Intercept, resp = x1),
+    prior(normal(0, 10), class = Intercept, resp = x2)),
+  sample_prior = T,
+  seed = 123,
+  chains = 2,
+  cores = 2,
+  iter = 2000,
+  backend = "cmdstanr",
+  threads = threading(2),
+  control = list(
+    max_treedepth = 20,
+    adapt_delta = 0.99)
+)
+
+stancode(m)
+  
