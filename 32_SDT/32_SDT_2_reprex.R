1edf9561dec461a4ddd848adf1e0aff27f7efb5b
fusaroli <fusaroli@gmail.com>
2021-09-29 11:00:42 +0200

    add reprex for sdt divergences

--- 32_SDT/32_SDT_2_reprex.R
+++ 32_SDT/32_SDT_2_reprex.R
@@ -1,43 +1,110 @@
-source(here("32_SDT", "heit_rotello.RData")) #loads the data
+library(cmdstanr)
+
+std_i <- matrix(
+  c(3,1,1,3,4,0,0,4,4,4,0,0,4,1,0,3,4,3,0,1,4,4,0,0,4,1,0,3,4,3,0,1,4,
+    0,0,4,4,2,0,2,3,2,1,2,4,2,0,2,4,0,0,4,3,4,1,0,3,4,1,0,4,1,0,3,3,2,
+    1,2,4,1,0,3,3,1,1,3,4,1,0,3,3,2,1,2,4,0,0,4,3,4,1,0,4,4,0,0,4,2,0,
+    2,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,0,0,4,2,3,2,1,4,0,0,4,4,4,0,0,
+    2,3,2,1,4,0,0,4,4,3,0,1,4,4,0,0,4,1,0,3,4,1,0,3,4,4,0,0),
+  nrow=40,ncol=4, byrow=T)
 
   
 hits_i <- std_i[, 1]
-hits_d <- std_d[, 1]
 falsealarms_i <- std_i[, 2]
-falsealarms_d <- std_d[, 2]
 missed_i <- std_i[, 3]
-missed_d <- std_d[, 3]
 correctrejections_i <- std_i[, 4]
-correctrejections_d <- std_d[, 4]
 
 signal_i <- hits_i + missed_i
-signal_d <- hits_d + missed_d
 noise_i <- falsealarms_i + correctrejections_i
-noise_d <- falsealarms_d + correctrejections_d
 signal_i <- signal_i[1]; 
-signal_d <- signal_d[1]; 
 noise_i <- noise_i[1] #Each subject gets same number of signal and noise trials 
-noise_d <- noise_d[1] 
 
 k_i <- nrow(std_i) 
-k_d <- nrow(std_d) 
-  
+
 data <- list(
   hits_i = hits_i, 
-  hits_d = hits_d, 
   falsealarms_i = falsealarms_i,
-  falsealarms_d = falsealarms_d, 
   signal_i = signal_i,
-  signal_d = signal_d, 
   noise_i = noise_i, 
-  noise_d = noise_d, 
-  k_i = k_i, 
-  k_d = k_d) # To be passed on to Stan
+  k_i = k_i) # To be passed on to Stan
 
+stan_file <- write_stan_file("
+// Signal detection theory model
+//
+
+// The input data 
+data {
   
+  int<lower=1> k_i; // n of participants
+  int<lower=0> signal_i; // cases
+  int<lower=0> hits_i[k_i]; // true positives
+  int<lower=0> noise_i; // non-casees
+  int<lower=0> falsealarms_i[k_i]; // false positives
   
-file <- file.path(here("32_SDT", "32_SDT_2.stan"))
-mod <- cmdstan_model(file, cpp_options = list(stan_threads = TRUE), pedantic = TRUE)
+}
+
+// The parameters accepted by the model.
+parameters {
+  
+  vector[k_i] criterion_i;
+  real criterionprior;
+  
+  vector[k_i] discriminability_i;
+  real discriminabilityprior;
+  
+  real mu_crit_i;
+  real mu_disc_i;
+  real muprior;
+  
+  real<lower=0> sigma_crit_i;
+  real<lower=0> sigma_disc_i;
+  real<lower=0> sigmaprior;
+  
+}
+
+transformed parameters{
+  real<lower=0,upper=1> theta_h_i[k_i];
+  real<lower=0,upper=1> theta_f_i[k_i];
+  
+  real<lower=0,upper=1> theta_h_prior;
+  real<lower=0,upper=1> theta_f_prior;
+  
+  // Reparameterization Using Equal-Variance Gaussian SDT
+  theta_h_prior = Phi(discriminabilityprior / 2 - criterionprior);
+  theta_f_prior = Phi(-discriminabilityprior / 2 - criterionprior);
+    
+  for(i in 1:k_i) {
+    theta_h_i[i] = Phi(discriminability_i[i] / 2 - criterion_i[i]);
+    theta_f_i[i] = Phi(-discriminability_i[i] / 2 - criterion_i[i]);
+  }
+  
+}
+
+// The model to be estimated. 
+model {
+
+  // These Priors over Discriminability and Bias Correspond 
+  // to Uniform Priors over the Hit and False Alarm Rates
+  mu_crit_i ~ normal(0, 5);
+  mu_disc_i ~ normal(0, 5);
+  muprior ~ normal(0, 5);
+  
+  sigma_crit_i ~ normal(0, 3);
+  sigma_disc_i ~ normal(0, 3);
+  sigmaprior ~ normal(0, 3);
+  
+  discriminability_i ~ normal(mu_disc_i, sigma_disc_i);
+  discriminabilityprior ~ normal(muprior, sigmaprior);
+  criterion_i ~ normal(mu_crit_i, sigma_crit_i);
+  criterionprior ~ normal(muprior, sigmaprior);
+  
+  // Observed counts
+  hits_i ~ binomial(signal_i, theta_h_i);
+  falsealarms_i ~ binomial(noise_i, theta_f_i);
+  
+}")
+  
+mod <- cmdstan_model(stan_file, cpp_options = list(stan_threads = TRUE), pedantic = TRUE)
 
 samples <- mod$sample(
   data = data,
@@ -45,30 +112,27 @@ samples <- mod$sample(
   chains = 2,
   parallel_chains = 2,
   threads_per_chain = 2,
-  iter_warmup = 5000,
-  iter_sampling = 5000,
-  refresh = 500,
+  iter_warmup = 1000,
+  iter_sampling = 1000,
+  refresh = 100,
   max_treedepth = 20,
   adapt_delta = 0.99,
 )
 
-draws_df <- as_draws_df(samples$draws()) 
+# Some basic quality checks of prior / posterior updates (was the prior too narrow/broad/badly positioned?)
 
+draws_df <- as_draws_df(samples$draws()) 
 
 ggplot(draws_df)+
   geom_density(aes(muprior), fill="red", alpha=0.3) +
   geom_density(aes(`mu_crit_i`), fill="blue", alpha=0.3) +
-  geom_density(aes(`mu_crit_d`), fill="blue", alpha=0.3) +
   geom_density(aes(`mu_disc_i`), fill="green", alpha=0.3) +
-  geom_density(aes(`mu_disc_d`), fill="green", alpha=0.3) +
   theme_classic()
 
 ggplot(draws_df)+
   geom_density(aes(sigmaprior), fill="red", alpha=0.3) +
   geom_density(aes(`sigma_crit_i`), fill="blue", alpha=0.3) +
-  geom_density(aes(`sigma_crit_d`), fill="blue", alpha=0.3) +
   geom_density(aes(`sigma_disc_i`), fill="green", alpha=0.3) +
-  geom_density(aes(`sigma_disc_d`), fill="green", alpha=0.3) +
   theme_classic()
 
 ggplot(draws_df)+
@@ -76,9 +140,6 @@ ggplot(draws_df)+
   geom_density(aes(`criterion_i[1]`), fill="blue", alpha=0.3) +
   geom_density(aes(`criterion_i[2]`), fill="blue", alpha=0.3) +
   geom_density(aes(`criterion_i[3]`), fill="blue", alpha=0.3) +
-  geom_density(aes(`criterion_d[1]`), fill="green", alpha=0.3) +
-  geom_density(aes(`criterion_d[2]`), fill="green", alpha=0.3) +
-  geom_density(aes(`criterion_d[3]`), fill="green", alpha=0.3) +
   theme_classic()
 
 ggplot(draws_df)+
@@ -86,37 +147,36 @@ ggplot(draws_df)+
   geom_density(aes(`discriminability_i[1]`), fill="blue", alpha=0.3) +
   geom_density(aes(`discriminability_i[2]`), fill="blue", alpha=0.3) +
   geom_density(aes(`discriminability_i[3]`), fill="blue", alpha=0.3) +
-  geom_density(aes(`discriminability_d[1]`), fill="green", alpha=0.3) +
-  geom_density(aes(`discriminability_d[2]`), fill="green", alpha=0.3) +
-  geom_density(aes(`discriminability_d[3]`), fill="green", alpha=0.3) +
   theme_classic()
 
+## Now quality checks of joint distributions against the prior
+
 ggplot(draws_df)+
-  geom_point(aes(criterionprior, discriminabilityprior), color="red", alpha=0.05) +
-  geom_point(aes(`criterion_i[1]`, `discriminability_i[1]`), color="green", alpha=0.1) +
+  geom_point(aes(criterionprior, sample(discriminabilityprior)), color="red", alpha=0.05) + # sample() is needed because of how I generated the prior samples (badly)
+  geom_point(aes(`criterion_i[1]`, `discriminability_i[1]`), color="blue", alpha=0.1) +
   geom_point(aes(`criterion_i[2]`, `discriminability_i[2]`), color="green", alpha=0.1) +
-  geom_point(aes(`criterion_d[1]`, `discriminability_d[1]`), color="blue", alpha=0.1) +
-  geom_point(aes(`criterion_d[2]`, `discriminability_d[2]`), color="blue", alpha=0.1) +
+  geom_point(aes(`criterion_i[3]`, `discriminability_i[3]`), color="purple", alpha=0.1) +
   theme_classic()
 
 ggplot(draws_df)+
   geom_point(aes(muprior, sigmaprior), color="red", alpha=0.05) +
   geom_point(aes(`mu_crit_i`, `sigma_crit_i`), color="green", alpha=0.1) +
   geom_point(aes(`mu_disc_i`, `sigma_disc_i`), color="blue", alpha=0.1) + 
-  geom_point(aes(`mu_crit_d`, `sigma_crit_d`), color="orange", alpha=0.1) +
-  geom_point(aes(`mu_disc_d`, `sigma_disc_d`), color="yellow", alpha=0.1) +
   theme_classic()
 
 ggplot(draws_df)+
   geom_point(aes(muprior, sample(muprior)), color="red", alpha=0.05) +
   geom_point(aes(`mu_crit_i`, `mu_disc_i`), color="green", alpha=0.1) +
-  geom_point(aes(`mu_crit_d`, `mu_disc_d`), color="blue", alpha=0.1) +
-  geom_point(aes(`mu_disc_i`, `mu_disc_d`), color="orange", alpha=0.1) + 
-  geom_point(aes(`mu_crit_i`, `mu_crit_d`), color="yellow", alpha=0.1) +
+  theme_classic()
+
+ggplot(draws_df)+
+  geom_point(aes(sigmaprior, sample(sigmaprior)), color="red", alpha=0.05) +
+  geom_point(aes(`sigma_crit_i`, `sigma_disc_i`), color="green", alpha=0.1) +
   theme_classic()
 
 
-# Extract diagnostics
+# Extract diagnostics for divergences
+
 samples$cmdstan_diagnose() # summary
 diagnostics_df <- as_draws_df(samples$sampler_diagnostics())
 print(diagnostics_df)
@@ -131,18 +191,8 @@ ggplot(draws_df)+
   theme_classic()
 
 ggplot(draws_df)+
-  geom_point(aes(`mu_crit_d`, `sigma_crit_d`), alpha=0.1) +
-  geom_point(data=x, aes(`mu_crit_d`, `sigma_crit_d`), color="red") +
-  theme_classic()
-
-ggplot(draws_df)+
   geom_point(aes(`mu_disc_i`, `sigma_disc_i`), alpha=0.1) +
   geom_point(data=x, aes(`mu_disc_i`, `sigma_disc_i`), color="red") +
-  theme_classic()
-
-ggplot(draws_df)+
-  geom_point(aes(`mu_disc_d`, `sigma_disc_d`), alpha=0.1) +
-  geom_point(data=x, aes(`mu_disc_d`, `sigma_disc_d`), color="red") +
   theme_classic()
 
 ggplot(draws_df)+
@@ -150,39 +200,19 @@ ggplot(draws_df)+
   geom_point(data=x, aes(`mu_crit_i`, `mu_disc_i`), color="red") +
   theme_classic()
 
-ggplot(draws_df)+
-  geom_point(aes(`mu_crit_d`, `mu_disc_d`), alpha=0.1) +
-  geom_point(data=x, aes(`mu_crit_d`, `mu_disc_d`), color="red") +
-  theme_classic()
 
-ggplot(draws_df)+
-  geom_point(aes(`mu_crit_i`, `mu_crit_d`), alpha=0.1) +
-  geom_point(data=x, aes(`mu_crit_i`, `mu_crit_d`), color="red") +
-  theme_classic()
-
+## Trace plots
 ggplot(draws_df, aes(.iteration, sigma_disc_i, color=as.factor(.chain))) +
   geom_line() +
   theme_classic()
-ggplot(draws_df, aes(.iteration, sigma_disc_d, color=as.factor(.chain))) +
-  geom_line() +
-  theme_classic()
 ggplot(draws_df, aes(.iteration, sigma_crit_i, color=as.factor(.chain))) +
-  geom_line() +
-  theme_classic()
-ggplot(draws_df, aes(.iteration, sigma_crit_d, color=as.factor(.chain))) +
   geom_line() +
   theme_classic()
 
 ggplot(draws_df, aes(.iteration, mu_disc_i, color=as.factor(.chain))) +
   geom_line() +
   theme_classic()
-ggplot(draws_df, aes(.iteration, mu_disc_d, color=as.factor(.chain))) +
-  geom_line() +
-  theme_classic()
 ggplot(draws_df, aes(.iteration, mu_crit_i, color=as.factor(.chain))) +
-  geom_line() +
-  theme_classic()
-ggplot(draws_df, aes(.iteration, mu_crit_d, color=as.factor(.chain))) +
   geom_line() +
   theme_classic()
 
@@ -194,12 +224,3 @@ ggplot(draws_df) +
   geom_rug(aes(`mu_disc_d`, `mu_crit_d`), color="blue", size=0.1, alpha=0.1) +
   theme_classic()
 
-### Now brms to the rescue
-
-df <- tibble(
-  hits = c(hits_i, hits_d),
-  falsealarms = c(falsealarms_i,falsealarms_d),
-  signal = c(hits_i + missed_i, hits_d + missed_d),
-  noise = c(falsealarms_i + correctrejections_i, falsealarms_d + correctrejections_d)) # To be passed on to Stan
-
-pivot_longer()
